d0ebc66393e984bc3184842b13dc2db68f0e6dfe
HebertMoraes <hebertmoraes2019@gmail.com>
2021-05-06 16:13:31 -0300

    iniciado a primeira versão de interpretação de Tiled em JSON

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
+import mapJSON from './assets/maps/map.json';
+import mapPNG from './assets/assetsmap.png';
+import waterPNG from './assets/water.png';
 
 class MyGame extends Phaser.Scene
 {
@@ -10,29 +13,37 @@ class MyGame extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('logo', logoImg);
+        //carrega esta imagem
+        this.load.image("background", waterPNG);
+        this.load.image("tiles", mapPNG);
+        //carrega este arquivo JSON
+        this.load.tilemapTiledJSON("map", mapJSON);
     }
       
     create ()
     {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
+        //simplesmente inserir uma imagem já carregada, não sei porque mas se passar parametro x & y como 0, ainda aparece
+        this.add.image(650, 650, "background");
+        //cria um mundo tiled e passa o mapa carregado anteriormente do qual é um JSON já carregado
+        const map = this.make.tilemap({key: "map"});
+        //insere a imagem carregada anteriormente como uma imagem que pode ser inserida no browser, e insere no lugar do assetsMap
+        //que é o nome dado ao tileSet lá no tiledMap JSON.
+        const tileset = map.addTilesetImage("assetsMap", "tiles");
+        
+        //Carrega, cria e aparentemente desenha a layer indicada pelo nome alí no parâmetro, nome este dado lá no tiledMap,
+        //imagino que para poder carregar a layer, deve-se estar carregado todos as imagens dos tiled's usados na layer, 
+        //e também inserido a imagem como tileSet, como na linha acima.
+        const ground = map.createLayer("ground", tileset, 0, 0)
+        const objectColliDer = map.createLayer("ObjectCollider", tileset, 0, 0)
+        const aboveCollider = map.createLayer("aboveObject", tileset, 0, 0)
     }
 }
 
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
+    width: 650,
+    height: 650,
     scene: MyGame
 };
 
